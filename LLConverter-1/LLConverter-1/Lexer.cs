901791498df408e54047c98f31e844c02abe7d30
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2024-04-03 14:43:20 +0300

    добавил в лексер примеры строк

--- LLConverter-1/LLConverter-1/Lexer.cs
+++ LLConverter-1/LLConverter-1/Lexer.cs
@@ -8,15 +8,23 @@ namespace LLConverter_1
 {
     internal class Lexer
     {
+        private string[] _str = ["type", "c", "=", "record", "a", ":", "int", ";", "b", ":", "int", "end", ";", "b", ":", "int"];
+        //private string[] _str = ["type", "c", "=", "record", "a", ":", "int", "end"];
+        //private string[] _str = ["type", "c", "=", "int"];
+        private int ptr = 0;
         public string GetNextToken()
         {
             if (IsEnd()) throw new Exception("Called GetNextToken() when Lexer IsEnd");
-            return "";
+            return _str[ptr++];
         }
 
         public bool IsEnd()
         {
-            return false;
+            return ptr >= _str.Length;
         }
     }
 }
+//S -> type I = T B 
+//T -> int | record I: T B end
+//B->e|; I: T B
+//I->a | b | c
