34b0542b61d8831424b3124438cbbd84497940cf
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2024-04-10 14:35:46 +0300

    поправил main

--- LLConverter-1/LLConverter-1/Lexer.cs
+++ LLConverter-1/LLConverter-1/Lexer.cs
@@ -8,9 +8,10 @@ namespace LLConverter_1
 {
     internal class Lexer
     {
-        //private string[] _str = ["type", "c", "=", "record", "a", ":", "int", ";", "b", ":", "int", "end", ";", "b", ":", "int"];
-        //private string[] _str = ["type", "c", "=", "record", "a", ":", "int", "end"];
-        private string[] _str = ["type", "c", "=", "int", "@"];
+        private string[] _str = ["type", "c", "=", "record", "a", ":", "int", "b", ":", "int", "end", "b", ":", "int", "@"];
+        //private string[] _str = ["type", "c", "=", "record", "a", ":", "int", ";", ";", "b", ":", "int", "end", ";", "b", ":", "int", "@"];
+        //private string[] _str = ["type", "c", "=", "record", "a", ":", "int", "end", "@"];
+        //private string[] _str = ["type", "c", "=", "int", "@"];
         private int ptr = 0;
         public string GetNextToken()
         {
--- LLConverter-1/LLConverter-1/Program.cs
+++ LLConverter-1/LLConverter-1/Program.cs
@@ -5,35 +5,22 @@ class Program
 {
     public static void Main(string[] args)
     {
-        if (args.Length != 2)
-        {
-            //throw new ArgumentException("Wrong arguments");
-        }
 
         FileParser fileParser = new("input.txt", true);
-        //{
-        //    GrammarRules = [
-        //   new GrammarRule("S", ["a", "A", "a", "B"], ["a"]),
-        //   //new GrammarRule("S", ["a"], ["a"]),
-        //   new GrammarRule("A", ["a", "A", "E"], ["a"]),
-        //   new GrammarRule("A", ["E"], ["e"]),
-        //   new GrammarRule("B", ["b", "B", "F"], ["b"]),
-        //   new GrammarRule("B", ["F"], ["f"]),
-        //   new GrammarRule("E", ["e"], ["e"]),
-        //   new GrammarRule("F", ["f"], ["f"]),
-        //]
-        //};
+        
         fileParser.ParseLinesToGrammarRules();
         LLTableBuilder builder = new();
         var table = builder.Build(fileParser.GrammarRules);
         TableSlider slider = new();
-        table.Write("out.csv");        
-        slider.RunSlider(table);
-        foreach (GrammarRule rule in fileParser.GrammarRules) 
+        table.Write("out.csv"); 
+        try
         {
-            Console.WriteLine(rule.Token);
-            Console.WriteLine(rule.DirectionSymbols.Count);
-            Console.WriteLine(rule.SymbolsChain.Count);
+            slider.RunSlider(table);
+            Console.WriteLine("all good");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
         }
     }
 }
