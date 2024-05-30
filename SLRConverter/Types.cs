7564f03f6e83da22477f562f026dcd92301658d9
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2024-05-30 10:40:38 +0300

    начал бегунок, поправил типы

--- SLRConverter/Types.cs
+++ SLRConverter/Types.cs
@@ -3,7 +3,7 @@
     public class GrammarRule(
         string token,
         List<string> symbolsChain,
-        List<string> directionSymbols
+        List<TableCell> directionSymbols
     )
     {
         // левый нетерминал правила
@@ -13,45 +13,18 @@
         public List<string> SymbolsChain { get; set; } = symbolsChain;
 
         // направляющее множество символов, с которых может начинаться правило
-        public List<TableCell> DirectionSymbols { get; set; }
+        public List<TableCell> DirectionSymbols { get; set; } = directionSymbols;
     }
 
     public struct TableCell
     {
-        public bool shift;
-        public int ponter;
+        public bool shift;//true - сдвиг, false - свертка
+        public int number;
     }
 
-    public class Column(
-        string token,
-        List<string> directionSymbols,
-        bool shift,
-        bool error = true,
-        int? pointer = null,
-        bool moveToNextLine = false,
-        bool end = false
-    )
+    public class Row
     {
-
-        public Dictionary<string, TableCell> Pointers { get; set; }
-
-        // нетерминал
-        //public string Token { get; set; } = token;
-
-        //// направляющее множество символов, с которых может начинаться правило
-        //public List<string> DirectionSymbols { get; set; } = directionSymbols;
-
-        //// сдвиг
-        //public bool Shift { get; set; } = shift;
-
-        //public bool Error { get; set; } = error;
-
-        //public int? Pointer { get; set; } = pointer;
-
-        //// переходить на следующую строку после разбора текущей строки или нет (заносить ли в стек)
-        //public bool MoveToNextLine { get; set; } = moveToNextLine;
-
-        //public bool End { get; set; } = end;
+        public Dictionary<string, TableCell> Cells { get; set; } = [];
     }
 
     public struct RowKey
@@ -64,13 +37,8 @@
 
     }
 
-    public class Table(List<Column> rows)
+    public class Table(List<Row> rows)
     {
-        //public RowKey root { get; set; }
-        public List<string> keys { get; set; }
-
-        public Dictionary<int, Column> Rows { get; set; }
-
-        //public List<Row> Rows { get; set; } = rows;
+        public List<Row> Rows { get; set; } = rows;
     }
 }
