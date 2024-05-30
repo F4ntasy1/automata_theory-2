8aac0c028ce96e34ef47b7a2a35abf9e4a6c2a7d
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2024-05-30 18:04:31 +0300

    сделал бегунок

--- SLRConverter/TableSlider.cs
+++ SLRConverter/TableSlider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,44 @@ namespace LLConverter_1
             string currToken = _lexer.GetNextToken();
             Row currRow = table.Rows[currRowNumber];
 
+            Stack<string> tempTokens = [];
+
+            stack.Push(currRowNumber);
+
             while (true)
             {
-                if (_lexer.IsEnd() && currRowNumber == 0 && stack.Count == 0 && currToken == table.RootName) return;
+                if (_lexer.IsEnd() && tempTokens.Count == 0 && currRowNumber == 0 && stack.Count == 0 && currToken == table.RootName) return;
+
+                if(currRow.Cells.TryGetValue(currToken, out TableCell cell))    
+                {
+                    if(cell.shift)
+                    {
+                        currRowNumber = cell.number;
+                    }
+                    else
+                    {
+                        for(int i = 0; i < table.GrammarRules[cell.number].SymbolsChain.Count; i++)
+                        {
+                            stack.Pop();
+                        }
+                        tempTokens.Push(table.GrammarRules[cell.number].Token);
+                    }
+                }
+                else
+                {
+                    throw new Exception($"given token: {currToken}, expected: " + string.Join(", ", currRow.Cells.Keys));
+                }
+
+                if(tempTokens.Count > 0)
+                {
+                    currToken = tempTokens.Pop();
+                }
+                else
+                {
+                    currToken = _lexer.GetNextToken();
+                }
+
+                currRow = table.Rows[currRowNumber];
             }
         }
     }
